c69cfa3fa4286a634dccf12a0895d804f9dc0c16
martigonchi <martigonchi@gmail.com>
2024-08-22 20:09:44 -0300

    Revert "cambios"

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-// Código JavaScript para iniciar sesión:
+//Código JavaScript para iniciar sesión://
 
 const ingresar = document.getElementById("ingresar");
 
@@ -8,10 +8,22 @@ ingresar.addEventListener("click", function() {
     const psw = document.getElementById('contrasena').value.trim();
     const alertDanger = document.getElementById('alert-danger');
 
-    // Validar si los campos están vacíos
-    if (user === '' || psw === '') {
+<<<<<<< HEAD
+if (valid) {
+    location.replace("index.html")
+} else {
+    document.getElementById("alert-danger").classList.add("show");
+}
+
+})
+=======
+    if (!user || !psw) {
         valid = false;
+        alertDanger.querySelector('p').textContent = 'Ningún campo puede estar vacío.';
+        alertDanger.classList.add('show');
+        return;
     }
+>>>>>>> fabi
 
     if (valid) {
         // Guardar la sesión en sessionStorage
@@ -19,19 +31,32 @@ ingresar.addEventListener("click", function() {
         // Redireccionar a la página principal
         location.replace("index.html");
     } else {
-        // Mostrar alerta si la validación falla
         document.getElementById("alert-danger").classList.add("show");
     }
 });
 
+<<<<<<< HEAD
+=======
+//Código de JavaScript para visualizar contraseña//
+
+>>>>>>> fabi
 const togglePassword = document.querySelector('#togglePassword');
 const contrasena = document.querySelector('#contrasena');
 
 togglePassword.addEventListener('click', function (e) {
+<<<<<<< HEAD
+// Alternar el tipo de input
+const type = contrasena.getAttribute('type') === 'password' ? 'text' : 'password';
+contrasena.setAttribute('type', type);
+
+// Cambiar el ícono
+this.classList.toggle('fa-eye-slash');
+=======
     // Alternar el tipo de input
     const type = contrasena.getAttribute('type') === 'password' ? 'text' : 'password';
     contrasena.setAttribute('type', type);
 
     // Cambiar el ícono
     this.classList.toggle('fa-eye-slash');
+>>>>>>> fabi
 });
