cb9bc9fb573d9194deaf7ac8cec320a7b868f78e
fsalazar95 <fabianasalazar95uy@gmail.com>
2024-08-22 13:18:39 -0300

    Probado cod de desafiate

--- js/login.js
+++ js/login.js
@@ -1,23 +1,26 @@
-const ingresar = document.getElementById("ingresar")
+//Código JavaScript para iniciar sesión://
 
-ingresar.addEventListener("click", function(){
-let valid = true; 
-const user = document.getElementById('usuario').value.trim();
-const psw = document.getElementById('contrasena').value.trim();
-const alertDanger = document.getElementById('alert-danger');
+const ingresar = document.getElementById("ingresar");
 
-if (!user || !psw ) {
-    valid = false;
-    alertDanger.querySelector('p').textContent = 'Ningún campo puede estar vacío.';
-    alertDanger.classList.add('show');
-    return;
-}
+ingresar.addEventListener("click", function() {
+    let valid = true; 
+    const user = document.getElementById('usuario').value.trim();
+    const psw = document.getElementById('contrasena').value.trim();
+    const alertDanger = document.getElementById('alert-danger');
 
-if (valid) {
-    location.replace("index.html")
-} else {
-    document.getElementById("alert-danger").classList.add("show");
-}
-})
+    if (!user || !psw) {
+        valid = false;
+        alertDanger.querySelector('p').textContent = 'Ningún campo puede estar vacío.';
+        alertDanger.classList.add('show');
+        return;
+    }
 
-
+    if (valid) {
+        // Guardar la sesión en sessionStorage
+        sessionStorage.setItem('sesionIniciada', 'true');
+        // Redireccionar a la página principal
+        location.replace("index.html");
+    } else {
+        document.getElementById("alert-danger").classList.add("show");
+    }
+});
